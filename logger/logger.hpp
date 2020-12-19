efc53d5376c43c2a9095312d1e5399dac044f128
Arsen Hovhannisyan <arsen_hovhannisyan@protonmail.com>
2020-12-19 19:40:42 +0400

    - Added initial directory structure.     - Added cmake files.

---
+++ logger/logger.hpp
@@ -0,0 +1,13 @@
+#ifndef __LOGGER_H__
+#define __LOGGER_H__
+
+class logger
+{
+public:
+    auto test_logger() {
+	return "test from logger class";
+    }
+};
+
+
+#endif // __LOGGER_H__
---
+++ logger_demo/main.cpp
@@ -0,0 +1,11 @@
+#include "logger.hpp"
+
+#include <iostream>
+
+
+int main()
+{
+    logger l;
+    std::cout << l.test_logger() << std::endl;
+    return 0;
+}
