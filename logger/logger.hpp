612722e18c17035923b57aef648f74eaec6838ec
Arsen Hovhannisyan <arsen_hovhannisyan@protonmail.com>
2020-12-23 22:55:45 +0400

    - Added message logging functionality.     - Added constructor of logger class.

--- logger/logger.hpp
+++ logger/logger.hpp
@@ -1,13 +1,52 @@
 #ifndef __LOGGER_H__
 #define __LOGGER_H__
 
+#include <array>
+#include <functional>
+#include <iostream>
+#include <string_view>
+
 class logger
 {
+private:
+
+	using str_levels = std::array<const char*, 9>;
+	using o_stream = std::reference_wrapper<std::ostream>;
+
 public:
-    auto test_logger() {
-	return "test from logger class";
-    }
+
+	enum class level : int {
+		none, fatal, critical,
+		error, warning, notice,
+		information, debug, trace
+	};
+
+private:
+	
+	static constexpr str_levels levels{ "NONE", "FATAL", "CRITICAL",
+					    "ERROR", "WARNING", "NOTICE",
+					    "INFORMATION", "DEBUG", "TRACE" };
+
+	level m_level;
+	o_stream m_out_stream;
+
+private:
+
+	std::ostream& stream() const noexcept;
+
+public:
+
+	static logger& get() noexcept;
+
+	void configure(const o_stream& os, level l);
+
+	void message(const std::string_view str) noexcept;
+
+public:
+
+	logger();
 };
 
+#include "logger.icpp"
 
 #endif // __LOGGER_H__
