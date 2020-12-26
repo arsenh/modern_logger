20e41de17658edb268a38fdb5414c9c71bb26252
Arsen Hovhannisyan <arsen_hovhannisyan@protonmail.com>
2020-12-26 21:53:39 +0400

    - Added variadic templates implementations of       main logging functions.

--- logger/logger.hpp
+++ logger/logger.hpp
@@ -1,10 +1,10 @@
 #ifndef __LOGGER_H__
 #define __LOGGER_H__
 
-#include <array>
 #include <functional>
 #include <iostream>
-#include <string_view>
+#include <optional>
+#include <string>
 
 class logger
 {
@@ -12,10 +12,11 @@ private:
 
 	using str_levels = std::array<const char*, 9>;
 	using o_stream = std::reference_wrapper<std::ostream>;
+	using deleter = std::optional<std::function<void(std::ostream&)>>;
 
 public:
 
-	enum class level : int {
+	constexpr enum class level : int {
 		none, fatal, critical,
 		error, warning, notice,
 		information, debug, trace
@@ -23,26 +24,54 @@ public:
 
 private:
 	
-	static constexpr str_levels levels{ "NONE", "FATAL", "CRITICAL",
-					    "ERROR", "WARNING", "NOTICE",
-					    "INFORMATION", "DEBUG", "TRACE" };
-
 	level m_level;
 	o_stream m_out_stream;
+	deleter m_deleter;
+	const char m_logger_sym = '%';
 
 private:
 
 	std::ostream& stream() const noexcept;
+	void check_stream() const;
 
 public:
 
 	static logger& get() noexcept;
 
-	void configure(const o_stream& os, level l);
+	void configure(level l, const o_stream& os, deleter del = deleter{});
 
-	void message(const std::string_view str) noexcept;
+	void string_builder(std::string& format) noexcept;
 
-public:
+	template <typename T, typename... Args>
+	void string_builder(std::string& format, T t, Args... args) noexcept;
+
+	template <typename... Args>
+	void fatal(const std::string& format, Args... args) noexcept;
+
+	template <typename... Args>
+	void critical(const std::string& format, Args... args) noexcept;
+
+	template <typename... Args>
+	void error(const std::string& format, Args... args) noexcept;
+	
+	template <typename... Args>
+	void warning(const std::string& format, Args... args) noexcept;
+	
+	template <typename... Args>
+	void notice(const std::string& format, Args... args) noexcept;
+	
+	template <typename... Args>
+	void information(const std::string& format, Args... args) noexcept;
+	
+	template <typename... Args>
+	void debug(const std::string& format, Args... args) noexcept;
+	
+	template <typename... Args>
+	void trace(const std::string& format, Args... args) noexcept;
+	
+	virtual ~logger();
+
+private:
 
 	logger();
 };
@@ -50,3 +79,5 @@ public:
 #include "logger.icpp"
 
 #endif // __LOGGER_H__
+
+
